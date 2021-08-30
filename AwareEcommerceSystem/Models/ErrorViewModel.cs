7741890ce90710bccc833f282e76b25448785686
HelaniW <helani.waidyarathne@xitricon.com>
2021-08-30 10:40:55 +0530

    Add project files.

---
+++ AwareEcommerceSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AwareEcommerceSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

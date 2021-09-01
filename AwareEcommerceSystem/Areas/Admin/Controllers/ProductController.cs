f7d13b16d2588840b48aa9c1dfa0657c5d821a4e
HelaniW <helani.waidyarathne@xitricon.com>
2021-09-01 12:43:25 +0530

    special tag

--- AwareEcommerceSystem/Areas/Admin/Controllers/ProductController.cs
+++ AwareEcommerceSystem/Areas/Admin/Controllers/ProductController.cs
@@ -1,16 +1,79 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AwareEcommerceSystem.Data;
+using AwareEcommerceSystem.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AwareEcommerceSystem.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class ProductController : Controller
     {
+        private ApplicationDbContext _db;
+        [Obsolete]
+        private IHostingEnvironment _he;
+
+        [Obsolete]
+        public ProductController(ApplicationDbContext db, IHostingEnvironment he)
+        {
+            _db = db;
+            _he = he;
+        }
+
         public IActionResult Index()
         {
+            return View(_db.Products.Include(c => c.ProductTypes).Include(f => f.SpecialTag).ToList());
+        }
+
+        //Get Create method
+        public IActionResult Create()
+        {
+            ViewData["productTypeId"] = new SelectList(_db.ProductTypes.ToList(), "Id", "ProductType");
+            ViewData["TagId"] = new SelectList(_db.SpecialTags.ToList(), "Id", "Name");
             return View();
         }
+
+        //Post Create method
+        [HttpPost]
+        [Obsolete]
+        public async Task<IActionResult> Create(Products product, IFormFile image)
+        {
+            if (ModelState.IsValid)
+            {
+                var searchProduct = _db.Products.FirstOrDefault(c => c.Name == product.Name);
+                if (searchProduct != null)
+                {
+                    ViewBag.message = "This product is already exist";
+                    ViewData["productTypeId"] = new SelectList(_db.ProductTypes.ToList(), "Id", "ProductType");
+                    ViewData["TagId"] = new SelectList(_db.SpecialTags.ToList(), "Id", "Name");
+                    return View(product);
+                }
+
+                if (image != null)
+                {
+                    var name = Path.Combine(_he.WebRootPath + "/Images", Path.GetFileName(image.FileName));
+                    await image.CopyToAsync(new FileStream(name, FileMode.Create));
+                    product.Image = "Images/" + image.FileName;
+                }
+
+                if (image == null)
+                {
+                    product.Image = "Images/noimage.PNG";
+                }
+                _db.Products.Add(product);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(product);
+        }
+
     }
 }
